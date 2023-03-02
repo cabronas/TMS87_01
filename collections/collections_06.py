984893f1588ec3085f2d3ebd28705e78ac95055f
Sloj <cabronian123@gmail.com>
2023-03-02 19:18:34 +0300

    fix2

--- collections/collections_06.py
+++ collections/collections_06.py
@@ -7,10 +7,10 @@
 """
 dict = {'a': 1, 'b': 2}
 print(dict)
-dict['alex']=42
+dict['alex'] = 42
 print(dict)
 print(dict['alex'])
-dict['alex']=55
+dict['alex'] = 55
 print(dict)
 dict.pop('alex')
 print(dict)
