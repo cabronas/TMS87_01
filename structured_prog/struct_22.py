bceaf03a1881dde5c5343490065de46572e2acf4
Sloj <cabronian123@gmail.com>
2023-03-10 13:07:14 +0300

    22

--- structured_prog/struct_22.py
+++ structured_prog/struct_22.py
@@ -12,9 +12,27 @@
 """
 import datetime
 import time
+
 t = datetime.datetime.today()
-trains = {
-    "Number": "00",
-    "ArrivalP": "AA",
-    "ArrivalTime": datetime.datetime()
-}
+trains = [
+    {
+        "Number": "00",
+        "toP": "Minsk",
+        "toTime": datetime.datetime(2022, 2, 2, 15, 0),
+        "fromP": "Moscow",
+        "fromTime": datetime.datetime(2022, 2, 2, 22, 0)
+    }
+    ,
+    {
+        "Number": "01",
+        "toP": "Moscow",
+        "toTime": datetime.datetime(2022, 2, 2, 15, 0),
+        "fromP": "Khabarovsk",
+        "fromTime": datetime.datetime(2022, 2, 3, 15, 0)
+    }
+]
+print(trains[0]["toTime"])
+for train in trains:
+    travelTime = (train["fromTime"] - train["toTime"]).total_seconds() / 60.0  # Minutes
+    if travelTime > 440:
+        print(*train.values())
