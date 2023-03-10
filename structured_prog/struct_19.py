508c36951bdff01560c7097f7598f567983b0878
Sloj <cabronian123@gmail.com>
2023-03-10 12:46:27 +0300

    19-21

--- structured_prog/struct_19.py
+++ structured_prog/struct_19.py
@@ -2,19 +2,30 @@
 Дана целочисленная квадратная матрица.
 Найти в каждой строке наибольший элемент и поменять его местами с элементом главной диагонали.
 """
-# arr = []
-# for i in range(3):
-#     column = []
-#     for j in range(3):
-#         column.append(int(input()))
-#     arr.append(column)
-#
-# # output and find max
-# max = arr[0][0]
-# for i in range(len(arr)):
-#     for j in range(len(arr[i])):
-#         print(arr[i][j], " ", end='')
-#         if arr[i][j] > max:
-#             max = arr[i][j]
-#     print()
-# # replace
+n = int(input("N = "))
+arr = []
+maxDict = {}
+for i in range(n):
+    column = []
+    for j in range(n):
+        column.append(int(input()))
+    arr.append(column)
+    maxDict.update({i: arr[i][0]})
+
+# output and find max
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j], " ", end='')
+
+        if maxDict[i] < arr[i][j]:
+            maxDict.update({i: arr[i][j]})
+    print()
+print()
+
+# replace
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            arr[i][j] = maxDict[i]
+        print(arr[i][j], " ", end='')
+    print()
